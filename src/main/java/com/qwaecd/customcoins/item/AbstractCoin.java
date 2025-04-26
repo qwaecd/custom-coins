704314e620339033ff6f3237e8c8c3dbea94b179
qwaecd <zhong6252@foxmail.com>
2025-04-26 13:43:43 +0800

    feat: 添加金币和钻石币验证功能及显示信息

--- src/main/java/com/qwaecd/customcoins/item/AbstractCoin.java
+++ src/main/java/com/qwaecd/customcoins/item/AbstractCoin.java
@@ -1,6 +1,9 @@
 package com.qwaecd.customcoins.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -21,19 +24,42 @@ public abstract class AbstractCoin extends Item {
     public @NotNull InteractionResult useOn(@NotNull UseOnContext context){
         Player player = context.getPlayer();
         if(player == null) return InteractionResult.PASS;
+        String playerName = player.getName().getString();
         ItemStack itemStack = context.getItemInHand();
-        if (getWhiteListPlayerName().contains(player.getName().getString())){
+        if (getWhiteListPlayerName().contains(playerName)){
             CompoundTag tag = itemStack.getOrCreateTag();
             if(tag.getBoolean("check")) return InteractionResult.PASS;
-            tag.putBoolean("check",true);
-            tag.putInt("CustomModelData",1);
-            itemStack.enchant(Enchantments.INFINITY_ARROWS,1);
+            putNBT(itemStack,player);
+
             if(player.level().isClientSide())
                 player.sendSystemMessage(Component.translatable(
                         "customcoins.coin_to_success"
                 ));
+
             return  InteractionResult.SUCCESS;
         }
         return InteractionResult.PASS;
+    }
+
+    public static void putNBT(ItemStack itemStack,Player player){
+        CompoundTag tag = itemStack.getOrCreateTag();
+        tag.putBoolean("check",true);
+        tag.putInt("CustomModelData",1);
+        itemStack.enchant(Enchantments.INFINITY_ARROWS,1);
+        String playerName = player.getName().getString();
+        ListTag loreList = new ListTag();
+        loreList.add(StringTag.valueOf(Component.Serializer.toJson(
+                Component.translatable("item.check_coin_display",playerName)
+                        .withStyle(ChatFormatting.AQUA)
+        )));
+        CompoundTag displayTag = itemStack.getOrCreateTagElement("display");
+        displayTag.put("Lore",loreList);
+//        displayTag.putString(
+//                "Name",
+//                Component.Serializer.toJson(
+//                        Component.translatable("item.check_coin_display",playerName)
+//                                .withStyle(ChatFormatting.LIGHT_PURPLE, ChatFormatting.ITALIC)
+//                )
+//        );
     }
 }
